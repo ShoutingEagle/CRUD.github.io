550a8b31c5f1bcda91043a8d9080e898507e37fe
ShoutingEagle <117582341+ShoutingEagle@users.noreply.github.com>
2023-07-05 04:51:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function validateForm () {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
@@ -48,26 +49,28 @@ function validateForm () {
 
 function showData (){
     var peopleList;
-    if(localStorage.getItem('peopleList') == null){
+    if(localStorage.getItem("peopleList") == null){
         peopleList = [];
     }
     else{
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
     }
 
     var html = "";
     peopleList.forEach(function (element, index){
         let indexupdate = index +1;
+        html += "<tr>"
         html += "<td>"+indexupdate+"</td>";
         html += "<td>"+element.name+"</td>";
         html += "<td>"+element.email+"</td>";
         html += "<td>"+element.gpa+"</td>";
         html += "<td>"+element.age+"</td>";
         html += "<td>"+element.degree+"</td>";
-        html += '<td><button onclick="deleteData('+index+')" class="">Delete</button><button onclick="updateData('+index+')" class="">Edit</button></td>';
+        html += '<td><button onclick="updateData('+index+')" class="edit-button"><span class="material-symbols-outlined">edit</span></button><button onclick="deleteData('+index+')" class="delete-button"><span class="material-symbols-outlined">delete</span></button></td>';
+        html += "</tr>";
     });
 
-    document.getElementById("data").innerHTML = html;
+    document.querySelector("#crudTable tbody").innerHTML = html;
             
 }
 
@@ -88,11 +91,11 @@ function addData(){
 
 
     var peopleList;
-    if(localStorage.getItem('peopleList')==null){
+    if(localStorage.getItem("peopleList")==null){
         peopleList = [];
     }
     else{
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
     }
 
     peopleList.push({
@@ -103,7 +106,7 @@ function addData(){
         degree : degree,
     });
 
-    localStorage.setItem('peopleList', JSON.stringify(peopleList));
+    localStorage.setItem("peopleList", JSON.stringify(peopleList));
     showData();
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
@@ -122,11 +125,11 @@ function deleteData(index){
         peopleList = [];
     }
     else{
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
     }
 
 
-    peopleList.slice(index,1);
+    peopleList.splice(index,1);
     localStorage.setItem("peopleList",JSON.stringify(peopleList));
     showData();
 }
@@ -142,7 +145,7 @@ function updateData(index){
         peopleList = [];
     }
     else{
-        peopleList = JSON.parse(localStorage.getItem('peopleList'));
+        peopleList = JSON.parse(localStorage.getItem("peopleList"));
     }
 
     document.getElementById("name").value = peopleList[index].name;
@@ -174,3 +177,50 @@ function updateData(index){
         }
     }
 }
+
+
+function search (){
+    let filter = document.getElementById("search").value.toUpperCase();
+    let crudTable = document.getElementById("crudTable");
+    let tr = crudTable.getElementsByTagName("tr");
+    for(let i=0;i<tr.length;i++){
+        let td = tr[i].getElementsByTagName('td')[1];
+        let email = tr[i].getElementsByTagName('td')[2];
+        let degree = tr[i].getElementsByTagName('td')[5];
+
+        if(td){
+            let textValue = td.textContent || td.innerHTML;
+            if(textValue.toUpperCase().indexOf(filter)>-1){
+                tr[i].style.display = "";
+            }
+            else{
+                tr[i].style.display = "none";
+            }
+        }
+
+        if(email){
+                let textValue = email.textContent || email.innerHTML;
+                if(textValue.toUpperCase().indexOf(filter)>-1){
+                    tr[i].style.display = "";
+                }
+                else{
+                    tr[i].style.display = "none";
+                }
+            
+        }
+
+
+        if(degree){
+                let textValue = degree.textContent || degree.innerHTML;
+                if(textValue.toUpperCase().indexOf(filter)>-1){
+                    tr[i].style.display = "";
+                }
+                else{
+                    tr[i].style.display = "none";
+                }
+            
+        }
+    }
+ 
+}
+
